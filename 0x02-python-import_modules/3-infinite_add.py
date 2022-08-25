8d49deccf9d8953b9ed005e8e33ffb657624d9f4
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-08-25 05:15:34 -0700

    __main chaange

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+from sys import argv
 
-if __name__ == "main":
-    from sys import argv
+if __name__ == "__main__":
     n = 1
     totalsum = 0
     for i in range(n, len(argv)):
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,5 +5,6 @@ if __name__ == "__main__":
     i = 0
     arr = dir(hidden_4)
     while (len(arr) > i):
-        print("{}".format(arr[i]))
+        if arr[i][0] != '_':
+            print("{}".format(arr[i]))
         i += 1
