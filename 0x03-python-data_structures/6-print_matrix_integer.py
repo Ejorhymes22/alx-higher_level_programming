37a886e3308d4dbec49da5a8b80e2c0492e19ae1
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-08-27 06:37:03 -0700

    space changed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,9 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return 0
     for i in matrix:
-        for x in i:
-            print("{:d}".format(x), end='')
+        for x in range(0, len(i)):
+            if x < len(i) - 1:
+                print("{:d} ".format(i[x]), end='')
+            else:
+                print("{:d}".format(i[x]), end='')
         print()
