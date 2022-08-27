12357de35b86bb501fa71856d890b44b761cc375
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-08-27 06:22:20 -0700

    none included

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """prints all integers of a list in reverse order"""
-
-    if my_list == None:
+    if my_list is None:
         return 0
     for i in range(len(my_list) - 1, -1, -1):
         print("{:d}".format(my_list[i]))
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    """prints a matrix of integer"""
+    if matrix is None:
+        return 0
     for i in matrix:
         for x in i:
             print("{:d}".format(x), end='')
