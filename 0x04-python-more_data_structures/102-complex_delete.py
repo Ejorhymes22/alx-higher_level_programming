531be1c90209315ec4c68e2f72814f787cf5a10c
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-08-31 18:27:18 -0700

    al

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     """deletes keys with a specific value in a dictionary"""
-    for i, k in a_dictionary.items():
-        if k == value:
-            x = i
-    if x in a_dictionary:
-        del a_dictionary[x]
+    z = 0
+    for i in range(0, len(a_dictionary)):
+        x = None
+        for i, k in a_dictionary.items():
+            if k == value:
+                x = i
+                break
+        if x is not None:
+            del a_dictionary[x]
     return a_dictionary
