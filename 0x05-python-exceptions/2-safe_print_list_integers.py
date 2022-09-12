a8f3c70095ca31629dcd1b99233efe85ced7ca7f
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-09-12 05:09:16 -0700

    all

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,7 +10,5 @@ def safe_print_list_integers(my_list=[], x=0):
             if i == x - 1:
                 break
             pass
-        #except IndexError:
-            #pass
     print()
     return j
