db795a83a859af0cecaf75b9fad910855bc986e2
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-09-13 07:35:56 -0700

    module comment added

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
 """
-Please refer to the documentation provided in the README.md,
+__doc__:Please refer to the documentation provided in the README.md,
 which can be found at gorpyter's PyPI URL: https://pypi.org/project/gorpyter/
 """
-#!/usr/bin/python3
+
+
 class Square:
     """Represents an empty square."""
     pass
