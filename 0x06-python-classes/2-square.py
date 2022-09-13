841ed3ffff85fd376c9f960ffc381d42c6f0324e
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-09-13 07:48:17 -0700

    elif added

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -17,6 +17,7 @@ class Square:
         """
         if type(size) != int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self._size = size
+        else:
+            self._size = size
