36f08acba7377f5765f0a784af5917e2d5220b2b
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-09-14 06:34:48 -0700

    whitespace removed

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -20,7 +20,7 @@ class Square:
         """size with correct value"""
         if type(value) != int:
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
