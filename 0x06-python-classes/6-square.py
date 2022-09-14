1e21db1cdd4ae0f7c5fc3c6a6aac95483bb1fd4b
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-09-14 09:28:13 -0700

    size at 0

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,8 +8,8 @@ class Square:
     """position and size of a square"""
     def __init__(self, size=0, position=(0, 0)):
         """defines a size and pposition"""
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
