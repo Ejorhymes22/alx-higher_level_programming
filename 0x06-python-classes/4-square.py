65278de3909739cf79cad537d222a3ca57686ce5
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-09-14 06:29:34 -0700

    area and print#

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,20 +5,24 @@ This class defines a square
 
 
 class Square:
-    """defines a square with setter"""
-    def __init__(self, size=0):
-        self.__size = size
-
+    """defines a square with setter""" 
     @property
     def size(self):
+        """size of square"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """size with correct value"""
         if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __init__(self, size=0):
+        """size of initial"""
+        self.__size = size
 
     def area(self):
         """arae of the self"""
