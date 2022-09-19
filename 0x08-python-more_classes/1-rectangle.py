0005b577c6dbe406004a68051fd1423848d8c215
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-09-19 03:34:20 -0700

    ifmain

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,40 +4,40 @@
 creates a rectangle
 """
 
+if __name__ == "__main__":
+    class Rectangle:
+        """
+        defines a rectangle
+        """
+        @property
+        def width(self):
+            """dfines the width"""
+            return self.__width
 
-class Rectangle:
-    """
-    defines a rectangle
-    """
-    @property
-    def width(self):
-        """dfines the width"""
-        return self.__width
+        @width.setter
+        def width(self, value):
+            """defines width"""
+            if type(value) != int:
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
 
-    @width.setter
-    def width(self, value):
-        """defines width"""
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+        @property
+        def height(self):
+            """dfines the height"""
+            return self.__height
 
-    @property
-    def height(self):
-        """dfines the height"""
-        return self.__height
+        @height.setter
+        def height(self, value):
+            """defines hegit"""
+            if type(value) != int:
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
 
-    @height.setter
-    def height(self, value):
-        """defines hegit"""
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def __init__(self, width=0, height=0):
-        """initializes a rectangle"""
-        self.__width = width
-        self.__height = height
+        def __init__(self, width=0, height=0):
+            """initializes a rectangle"""
+            self.width = width
+            self.height = height
