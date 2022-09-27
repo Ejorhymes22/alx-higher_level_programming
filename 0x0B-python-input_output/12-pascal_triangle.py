8e2fd09a1cc2fbe114da303ae1a8aad8d36ef779
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-09-27 10:10:47 -0700

    pascalize

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+pascal triangle repr
+"""
+
+
+def pascal_triangle(n):
+    """
+    returns a list of list
+    of integers repr
+    pascal triange
+    Args:
+        n: the int
+    """
+    p, i, j, past, outer = 1, 0, 1, [1], [[1]]
+
+    while j < n:
+        inner_list = []
+        while i < p:
+            if i == 0:
+                inner_list.append(past[i])
+            if i < len(past) - 1:
+                inner_list.append(past[i] + past[i + 1])
+            if i == len(past) - 1:
+                inner_list.append(past[i])
+            i += 1
+        past = inner_list
+        outer.append(inner_list)
+        i = 0
+        p = p + 1
+        j += 1
+    return outer
