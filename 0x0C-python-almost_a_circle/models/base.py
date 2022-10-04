d5c9dc9c3df3c8a6d75e9f828847c71cbb78b884
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-10-04 07:46:27 -0700

    dummy fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -49,6 +49,8 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
+        from models.rectangle import Rectangle
+        from models.square import Square
         if cls.__name__ == "Rectangle":
             dummy = cls(1, 1, 2)
         elif cls.__name__ == "Square":
