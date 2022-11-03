a09c291e94634a5aa62b8644347409bb4c065e68
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-11-03 10:00:37 -0700

    all

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- lists all show contained in a database with one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+WHERE tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
