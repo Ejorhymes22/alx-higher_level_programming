fc1bc39ceb41d4d73326f638c940a5d55ae6e20f
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-12-02 08:15:30 -0800

    filter

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -4,18 +4,20 @@ if __name__ == "__main__":
     import sys
     import MySQLdb
 
-
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    stateName=sys.argv[4]
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    stateName = sys.argv[4]
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities WHERE cities.state_id=(SELECT id FROM states WHERE states.name=%s)", [stateName])
-    l = cur.fetchall()
+    cur.execute("SELECT cities.name FROM cities WHERE \
+                cities.state_id=(SELECT id FROM states \
+                WHERE states.name=%s)", [stateName])
+    stateList = cur.fetchall()
 
     z = 1
-    for i in l:
+    for state in stateList:
         k = ''
-        for j in i:
-            k += j
+        for letter in state:
+            k += letter
         if (z == 1):
             print(k, end="")
         else:
