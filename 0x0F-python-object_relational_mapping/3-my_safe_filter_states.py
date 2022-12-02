74fe3f87889a7fc036fd29a0563aa243e75f6c55
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-12-02 01:53:27 -0800

    all

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""selecting by state that start with N"""
+"""selecting a particular state"""
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name regexp '^N' ORDER BY id ASC")
-    mylist = cur.fetchall()
-    for i in mylist:
-        print(i)
+    element = sys.argv[4]
+    cur.execute("SELECT * FROM states \
+                WHERE name=%s ORDER BY id ASC", [element])
+    listcur = cur.fetchone()
+    print(listcur)
 
     cur.close()
     db.close()
