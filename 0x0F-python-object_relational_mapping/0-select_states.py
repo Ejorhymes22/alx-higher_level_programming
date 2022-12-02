799e5b549076861fc6daa5499db7abe7f17b1b10
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-12-02 01:11:06 -0800

    not importable

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,14 +4,14 @@
 import MySQLdb
 import sys
 
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    myresult = cur.fetchall()
+    for i in myresult:
+        print(i)
 
-db = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                     passwd=sys.argv[2], db=sys.argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-myresult = cur.fetchall()
-for i in myresult:
-    print(i)
-
-cur.close()
-db.close()
+    cur.close()
+    db.close()
