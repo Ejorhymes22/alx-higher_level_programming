f4f6254cd714c26d1c2408d7aaa1b2cddeb0d8e9
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-12-16 11:49:04 -0800

    pycode

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
     html = res.read()
 
 print('Body response:')
-print('\t-type: ' ,type(html))
-print('\t-content: ' ,html)
+print('\t-type: ', type(html))
+print('\t-content: ', html)
 if res.headers.get_content_charset() == 'utf-8':
     print('\t-utf8 content: OK')
