8c1648580261efc1dea144d3c0d36e165d0176dc
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-12-16 08:59:09 -0800

    $1"

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #displays only body of response
-curl -sI ubuntu@54.210.85.166 -O i.txt; if grep -q "200 OK" i.txt; then curl ubuntu@54.210.85.166; fi
+curl -sI "$1" -O i.txt; if grep -q "200 OK" i.txt; then curl "$1"; fi
