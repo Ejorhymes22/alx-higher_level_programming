160303c1a1e8202214c6ebd39f75c587a7725335
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-12-16 12:06:22 -0800

    decode

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,5 +11,4 @@ if __name__ == "__main__":
     print('Body response:')
     print('\t- type: {}'.format(type(html)))
     print('\t- content: {}'.format(html))
-    if res.headers.get_content_charset() == 'utf-8':
-        print('\t- utf8 content: OK')
+    print('\t- utf8 content: ', html.decode())
