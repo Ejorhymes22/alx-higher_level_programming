faa01474497a0fa823ee0a8050662bc272c9398b
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-12-18 06:48:47 -0800

    all

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     r = requests.get('https://api.github.com/repos/' + sys.argv[2] + '/'
                      + sys.argv[1] + '/commits', params={'per_page': 10})
     for item in r.json():
-        print("{} {}".format(item['sha'], item['commit']['author']['name']))
+        print("{}: {}".format(item['sha'], item['commit']['author']['name']))
