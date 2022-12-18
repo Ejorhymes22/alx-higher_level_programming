c5a09016f9f8172f1ea04b93fdca24f0e8c96299
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-12-18 06:42:57 -0800

    all

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# this takes 2 argumernt and list 10 commit s on github
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    r = requests.get('https://api.github.com/repos/' + sys.argv[2] + '/' + sys.argv[1] + '/commits', params={'per_page': 10})
+    for item in r.json():
+        print("{} {}".format(item['sha'], item['commit']['author']['name']))
